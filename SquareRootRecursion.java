0c2fcfd4d4e0990d065e3015819d44dfb66dc305
Ehimare Agboneni <eagbnoneni@albany.edu>
2017-02-25 20:55:53 -0500

    Added sqrt method to the class and main method

--- SquareRootRecursion.java
+++ SquareRootRecursion.java
@@ -1,3 +1,17 @@
 public class SquareRootRecursion{
+  
+  public static double babylSqrt(double x, double sqrt, double error){ 
+    double newSqrt
+    if(error <= ((sqrt*sqrt)-x){
+      return sqrt;
+    }
+    else{
+    newSqrt = 0.5*(sqrt + x / aqrt);
+    return babylSqrt(x, sqrt, error);
+    }
+  }
+public static void main(String[]args){
+
+}
 
 }
